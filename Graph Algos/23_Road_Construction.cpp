3bca65d7d89f9523922d818f3822313bafd49fb0
Pranay-Koyalkondwar <nomercy0018@gmail.com>
2026-09-20 11:54:02 +0530

    Adding new solution

---
+++ Graph Algos/23_Road_Construction.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+#define pb push_back
+
+const int MOD = 1000000000 + 7;
+int N = 1000000;
+const int INF = INT_MAX;
+
+struct UnionFind {
+    int n, set_size, *parent, *rank;
+    UnionFind() {}
+    UnionFind(int a) {
+        n = a; set_size = a;
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (x != parent[x])
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void merge(int x, int y) {
+        int xroot = find(x), yroot = find(y);
+        if (xroot != yroot) {
+            if (rank[xroot] >= rank[yroot]) {
+                rank[xroot] += rank[yroot];
+                parent[yroot] = xroot;
+            } else {
+                rank[yroot] += rank[xroot];
+                parent[xroot] = yroot;
+            }
+            set_size--;
+        }
+    }
+
+    int compsize(int x) {
+        return rank[find(x)];
+    }
+
+    void reset() {
+        set_size = n;
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int size() { return set_size; }
+
+    void print() {
+        for (int i = 1; i <= n; i++)
+            cout << i << " -> " << parent[i] << endl;
+    }
+};
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    UnionFind dsu(n);
+    int mx = 0;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        if (dsu.find(u) != dsu.find(v))
+        {
+            dsu.merge(u, v);
+        }
+        mx = max(mx, dsu.compsize(u));
+        cout << dsu.size() << " " << mx << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
